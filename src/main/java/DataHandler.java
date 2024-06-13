06f6432579279f93179633bfc7f23f4b2e22c6cd
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-06-13 10:35:19 +0200

    Initial commit

---
+++ src/main/java/DataHandler.java
@@ -0,0 +1,17 @@
+
+/*
+    Klasse som skal håndtere all datahåndtering til og fra database og
+    data fra scanner
+ */
+public class DataHandler
+{
+    //# Fields
+    private Database database;
+
+    //# Constructor
+    public DataHandler(Database database) {
+        this.database = database;
+    }
+
+
+}

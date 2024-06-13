264af4ae810164a76262f280efd28a16214c4582
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-06-13 17:11:13 +0200

    Finished loading all data from database when starting program

--- src/main/java/history_database/DataHandler.java
+++ src/main/java/history_database/DataHandler.java
@@ -10,12 +10,15 @@ import java.util.ArrayList;
     Klasse som skal håndtere all datahåndtering til og fra database og
     data fra scanner
  */
+
 public class DataHandler
 {
     //# Fields
     private Database database;
     private FileScanner input;
     private ArrayList<Person> peopleInDatabase = new ArrayList<>();
+    private ArrayList<Museum> museumsInDatabase = new ArrayList<>();
+    private ArrayList<FoundItem> itemsInDatabase = new ArrayList<>();
 
 
     //# Constructor
@@ -33,12 +36,15 @@ public class DataHandler
 
         for (Person person : people) {
 
+
         }
     }
 
     // Methods for loading data from database into program at start
     public void loadDataFromDatabaseAtStart() {
         loadPeopleFromDatabase();
+        loadMuseumsFromDatabase();
+        loadItemsFromDatabase();
     }
 
     public void loadPeopleFromDatabase() {
@@ -63,6 +69,111 @@ public class DataHandler
 
         } catch (SQLException e) {
             System.out.println("Could not load people from database");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadMuseumsFromDatabase() {
+        try (Connection connection = database.getConnection()) {
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM museum");
+
+            while (resultSet.next()) {
+                var museum = new Museum(
+                        resultSet.getInt("id"),
+                        resultSet.getString("navn"),
+                        resultSet.getString("sted")
+                );
+
+                museumsInDatabase.add(museum);
+            }
+
+            System.out.println(STR."//$ \{museumsInDatabase.size()} museums loaded from database");
+
+        } catch (SQLException e) {
+            System.out.println("Could not load people from database");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadItemsFromDatabase() {
+        try (Connection connection = database.getConnection()) {
+
+            Statement statementC = connection.createStatement();
+
+            // Load coins
+            ResultSet resultCoins = statementC.executeQuery("SELECT * FROM mynt");
+
+            while (resultCoins.next()) {
+                var coin = new ItemCoin(
+                        resultCoins.getInt("id"),
+                        resultCoins.getString("funnsted"),
+                        resultCoins.getInt("finner_id"),
+                        resultCoins.getString("funntidspunkt"),
+                        resultCoins.getInt("antatt_aarstall"),
+                        resultCoins.getInt("museum_id"),
+                        "Mynt",
+                        resultCoins.getInt("diameter"),
+                        resultCoins.getString("metall")
+                );
+                itemsInDatabase.add(coin);
+            }
+
+            // Load weapons
+            Statement statementW = connection.createStatement();
+
+            ResultSet resultWeapons = statementW.executeQuery("SELECT * FROM vaapen");
+
+            while (resultWeapons.next()) {
+                var weapon = new ItemWeapon(
+                        resultWeapons.getInt("id"),
+                        resultWeapons.getString("funnsted"),
+                        resultWeapons.getInt("finner_id"),
+                        resultWeapons.getString("funntidspunkt"),
+                        resultWeapons.getInt("antatt_aarstall"),
+                        resultWeapons.getInt("museum_id"),
+                        "Våpen",
+                        resultWeapons.getString("type"),
+                        resultWeapons.getString("materiale"),
+                        resultWeapons.getInt("vekt")
+                );
+
+                System.out.println(weapon);
+
+                itemsInDatabase.add(weapon);
+            }
+
+            // Load jewelry
+            Statement statementJ = connection.createStatement();
+
+            ResultSet resultJewelry = statementJ.executeQuery("SELECT * FROM smykke");
+
+            while (resultJewelry.next()) {
+                var jewelry = new ItemJewelry(
+                        resultJewelry.getInt("id"),
+                        resultJewelry.getString("funnsted"),
+                        resultJewelry.getInt("finner_id"),
+                        resultJewelry.getString("funntidspunkt"),
+                        resultJewelry.getInt("antatt_aarstall"),
+                        resultJewelry.getInt("museum_id"),
+                        "Smykke",
+                        resultJewelry.getString("type"),
+                        resultJewelry.getInt("verdiestimat"),
+                        resultJewelry.getString("filnavn")
+                );
+
+                System.out.println(jewelry);
+
+                itemsInDatabase.add(jewelry);
+            }
+
+
+            System.out.println(STR."//$ \{itemsInDatabase.size()} items loaded from database");
+
+        } catch (SQLException e) {
+            System.out.println("Could not load items from database");
             throw new RuntimeException(e);
         }
     }

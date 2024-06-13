4bf9bca674c3ac84eeff97b4889e00754ada015e
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-06-13 12:08:52 +0200

    Added fields, constructor and getters

--- src/main/java/history_database/FoundItem.java
+++ src/main/java/history_database/FoundItem.java
@@ -4,5 +4,50 @@ public abstract class FoundItem
 {
     //# Fields
     protected int id;
-    protected double x
+    protected String placeDiscovered;
+    protected int finder_id;
+    protected String dateFound;
+    protected int expectedYearOfCreation;
+    protected int museum_id;
+    protected String type;
+
+    //# Constructor
+    public FoundItem(int id, String placeDiscovered, int finder_id, String dateFound, int expectedYearOfCreation, int museum_id, String type) {
+        this.id = id;
+        this.placeDiscovered = placeDiscovered;
+        this.finder_id = finder_id;
+        this.dateFound = dateFound;
+        this.expectedYearOfCreation = expectedYearOfCreation;
+        this.museum_id = museum_id;
+        this.type = type;
+    }
+
+    //# Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getPlaceDiscovered() {
+        return placeDiscovered;
+    }
+
+    public int getFinder_id() {
+        return finder_id;
+    }
+
+    public String getDateFound() {
+        return dateFound;
+    }
+
+    public int getExpectedYearOfCreation() {
+        return expectedYearOfCreation;
+    }
+
+    public int getMuseum_id() {
+        return museum_id;
+    }
+
+    public String getType() {
+        return type;
+    }
 }

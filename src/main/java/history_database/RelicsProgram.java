9492852ee21dfd6b233bc587ef872de181f21b53
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-06-13 18:24:55 +0200

    Added menu for user and possible to write choice

--- src/main/java/history_database/RelicsProgram.java
+++ src/main/java/history_database/RelicsProgram.java
@@ -15,9 +15,53 @@ public class RelicsProgram
 
     //# Methods
     public void startProgram() {
-        System.out.println("Program is running...");
+        System.out.println("Program is booting up...");
 
         data.loadDataFromDatabaseAtStart();
         data.parseFile();
+        data.addNewDataToDatabase();
+
+        System.out.println("Program finished loading, starting now...");
+        printDivider();
+
+        showMenu();
+    }
+
+    private void showMenu() {
+        System.out.println("*** HOVEDMENY ***");
+        System.out.println("1. Se informasjon om alle gjenstander funnet.");
+        System.out.println("2. Se informasjon om alle gjenstander over en viss alder.");
+        System.out.println("3. Se informasjon om antall gjenstander registrert.");
+        System.out.println("4. Avslutt programmet.");
+        System.out.print("Skriv inn ditt menyvalg her: ");
+
+        int userInput = checkInputIfValidNumber(4);
+
+
+    }
+
+    private void printDivider() {
+        System.out.println("\n----------\n");
+    }
+
+    /*
+       Denne koden er basert på getNumberFromTerminalInput() fra foreleser Marcus Alexander Dahl i prosjektet Terminal
+       Link: https://github.com/kristiania/PGR112v24/blob/main/code/solutions/database/terminal/src/Terminal.java
+     */
+    public int checkInputIfValidNumber(int maxMenuNumber) {
+        try {
+            int userInput = Integer.parseInt(input.nextLine());
+            if (userInput <= maxMenuNumber) {
+                return userInput;
+            } else {
+                System.out.println(STR."Menyen har kun valg mellom 1-\{maxMenuNumber}. Prøv igjen: ");
+
+                return checkInputIfValidNumber(maxMenuNumber);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(STR."Menyen støtter kun tall mellom 1-\{maxMenuNumber}. Prøv igjen: ");
+
+            return checkInputIfValidNumber(maxMenuNumber);
+        }
     }
 }

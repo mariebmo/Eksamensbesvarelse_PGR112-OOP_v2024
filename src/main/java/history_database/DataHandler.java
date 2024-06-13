2b285d3060f4344c671f7c7bc3d14093c360d87c
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-06-13 11:02:12 +0200

    Set up startup method

--- src/main/java/history_database/DataHandler.java
+++ src/main/java/history_database/DataHandler.java
@@ -1,5 +1,7 @@
 package history_database;
 
+import history_database.data_in_out.MyScanner;
+
 /*
     Klasse som skal håndtere all datahåndtering til og fra database og
     data fra scanner
@@ -8,11 +10,12 @@ public class DataHandler
 {
     //# Fields
     private Database database;
+    private MyScanner input;
 
     //# Constructor
     public DataHandler(Database database) {
         this.database = database;
+        input = new MyScanner(database);
     }
-
 
 }
--- src/main/java/history_database/Main.java
+++ src/main/java/history_database/Main.java
@@ -8,15 +8,9 @@ public class Main
     static final Database database = new Database("src/main/resources/database.properties");
 
     public static void main(String[] args) {
-        DataHandler data = new DataHandler(database);
+        var data = new DataHandler(database);
+        var program = new RelicsProgram(data);
 
-
-        try (Connection connection = database.getConnection()) {
-            System.out.println("Database connected :) !");
-        } catch (SQLException e) {
-            System.out.println("Database not connected");
-            throw new RuntimeException(e);
-        }
-
+        program.startProgram();
     }
 }

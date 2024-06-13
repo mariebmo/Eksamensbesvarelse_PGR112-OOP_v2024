7f7a20063e0409db37093455ba3f4b845f036982
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-06-13 15:57:10 +0200

    Added method for loading data from person table in database at start

--- src/main/java/history_database/DataHandler.java
+++ src/main/java/history_database/DataHandler.java
@@ -1,5 +1,11 @@
 package history_database;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /*
     Klasse som skal håndtere all datahåndtering til og fra database og
     data fra scanner
@@ -8,16 +14,57 @@ public class DataHandler
 {
     //# Fields
     private Database database;
-    private MyScanner input;
+    private FileScanner input;
+    private ArrayList<Person> peopleInDatabase = new ArrayList<>();
+
 
     //# Constructor
     public DataHandler(Database database) {
         this.database = database;
-        input = new MyScanner(database);
+        input = new FileScanner(database);
     }
 
     void parseFile() {
         input.readFile();
+    }
+
+    public void addNewPeopleToDatabase() {
+        var people = input.getPeople();
+
+        for (Person person : people) {
+
+        }
+    }
+
+    // Methods for loading data from database into program at start
+    public void loadDataFromDatabaseAtStart() {
+        loadPeopleFromDatabase();
+    }
+
+    public void loadPeopleFromDatabase() {
+        try (Connection connection = database.getConnection()) {
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
+
+            while (resultSet.next()) {
+                var person = new Person(
+                        resultSet.getInt("id"),
+                        resultSet.getString("navn"),
+                        resultSet.getInt("tlf"),
+                        resultSet.getString("e_post")
+                );
+
+                peopleInDatabase.add(person);
+            }
+
+            System.out.println(STR."//$ \{peopleInDatabase.size()} people loaded from database");
+
+        } catch (SQLException e) {
+            System.out.println("Could not load people from database");
+            throw new RuntimeException(e);
+        }
     }
 
 }
